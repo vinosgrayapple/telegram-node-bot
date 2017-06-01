2d6fcc5dcce91ff57897ccac62387556039abc4a
Komarichev Serg <komarichev@gmail.com>
2017-06-01 16:49:37 +0300

    edit bot2.js 01/06/2017

--- bot2.js
+++ bot2.js
@@ -1,29 +1,40 @@
 const TelegramBot = require('node-telegram-bot-api');
 const request = require('request');
 const fs = require('fs');
-// const usersArray = require('./db').;
-// console.log( fs.readFileSync('./db.json'));
 var usersArray = JSON.parse(fs.readFileSync('./db.json')).json;
-// console.log(usersArray)
 const token = '388795002:AAE1UCy5COY_cOFeLSo0dFFJIlkU7mgkr4w';
-const whiteList = [122410782];
-// Create a bot that uses 'polling' to fetch new updates
+const whiteList = [122410782,340449760];
+var date = new Date();
+
 const bot = new TelegramBot(token, {polling: true});
 
 
 bot.onText(/\/audio/, function onAudioText(msg) {
+	const chatId = msg.chat.id;
+	if (whiteList.indexOf(chatId) == -1 ) return;
 	const stream = fs.createReadStream(`${__dirname}/audio/Kat.mp3`);
-	bot.sendAudio(msg.chat.id, stream);
+	bot.sendAudio(chatId, stream);
 });
 
+bot.onText(/\/pass (.+)/, (msg, match) => {
+	const chatId = msg.chat.id;
+	if (whiteList.indexOf(chatId) == -1 ) return;
+	const resp = match[1]
+	if (resp === "password") {
+		bot.sendMessage(chatId, "Вы в системе");
+	} else {
+		bot.sendMessage(chatId, "Не пиши сюда больше!!!");
+	}
+});
 
-bot.onText(/(.+)/, (msg, match) => { //\/in 
+bot.onText(/(.+)/, (msg, match) => { 
 	
   const chatId = msg.chat.id;
-  // console.log("chatId", chatId);
-  if (whiteList.indexOf(chatId) == -1 ) return;
+  console.log("chatId", chatId,"  ",(new Date()).toString().split(' ')[4]);
+  if (whiteList.indexOf(chatId) == -1) return;
+
   // console.log('chatId', chatId);
-  const resp = match[1].replace(/ /g,'');
+  const resp = match[1].replace(/\s/g,'');
 
   	filteredUser = usersArray.filter(user => user.lastName.toUpperCase().includes(resp.toUpperCase()));
 
@@ -31,7 +42,7 @@ bot.onText(/(.+)/, (msg, match) => { //\/in
 	(user) => {
 		const msgForSend = `_ ${user.lastName.toUpperCase()} ${user.firstName.toUpperCase()} _ :  
 								 📱 *${user.mobileNumber}*   (☎️ *${user.innerNumber}*)`;
-		const photo = fs.existsSync(`${__dirname}/img/${user.photo}`) ? `${__dirname}/img/${user.photo}` : "https://telegram.org/img/t_logo.png";
+		// const photo = fs.existsSync(`${__dirname}/img/${user.photo}`) ? `${__dirname}/img/${user.photo}` : "https://telegram.org/img/t_logo.png";
 		
 		bot.sendMessage(chatId, msgForSend,{"parse_mode": "Markdown" });
 	}
@@ -44,6 +55,7 @@ bot.onText(/(.+)/, (msg, match) => { //\/in
 bot.onText(/\/inall/, (msg, match) => {
   const chatId = msg.chat.id;
   if (whiteList.indexOf(chatId) == -1 ) return;
+  if (whiteList.indexOf(chatId) == -1 ) return;
 	msgForSend = usersArray.reduce(	(concat, user) =>  concat + `_ ${user.lastName.toUpperCase()} ${user.firstName.toUpperCase()} _ : ☎️ *${user.innerNumber}*  📱 *${user.mobileNumber}* \n\n` ,
 	``);
 	bot.sendMessage(chatId, msgForSend,{"parse_mode": "Markdown" });
@@ -51,8 +63,10 @@ bot.onText(/\/inall/, (msg, match) => {
 
 // Get HELP
 bot.onText(/\/help/, (msg, match) => {
+
   const chatId = msg.chat.id;
   if (whiteList.indexOf(chatId) == -1 ) return;
+
 	msgForSend = `
 	/help    - Помощь по командам бота
 	/inall   - Получить всех пользователей одним списком
@@ -72,9 +86,9 @@ console.log('Bot starting....')
 
 // Listen for any kind of message. There are different kinds of
 // messages.
-bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
+// bot.on('message', (msg) => {
+//   const chatId = msg.chat.id;
 
-  // send a message to the chat acknowledging receipt of their message
- // bot.sendMessage(chatId, 'Received your message');
-});
+//   // send a message to the chat acknowledging receipt of their message
+//  // bot.sendMessage(chatId, 'Received your message');
+// });
